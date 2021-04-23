a51809c90c1b2ddc2d2ceea4f7075d28fc8db7da
Despoina Sifouna <Despoina.Sifouna@kaluza.com>
2021-04-23 12:15:15 +0100

    create server

--- src/main/scala/com/example/chatapp/Main.scala
+++ src/main/scala/com/example/chatapp/Main.scala
@@ -1,8 +1,26 @@
 package com.example.chatapp
 
 import cats.effect.{ExitCode, IO, IOApp}
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
+import org.http4s.server.Router
+import org.http4s.server.blaze.BlazeServerBuilder
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends IOApp {
-  def run(args: List[String]) =
-    ChatappServer.stream[IO].compile.drain.as(ExitCode.Success)
+  def run(args: List[String]) : IO[ExitCode] = {
+    val helloWorldService = HttpRoutes.of[IO] {
+      case GET -> Root / "hello" / name =>
+        Ok(s"Hello, $name.")
+    }
+    val httpApp = Router("/" -> helloWorldService).orNotFound
+    BlazeServerBuilder[IO](global).bindHttp(8080, "localhost")
+      .withHttpApp(httpApp)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
+  }
 }
