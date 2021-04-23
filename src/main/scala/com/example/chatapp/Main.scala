264a58318d895d9ab91abf503fdedc6e7343399a
andrewinci <andrea.vinci@ovoenergy.com>
2021-04-23 16:23:16 +0100

    Forward any message to the other queues

--- src/main/scala/com/example/chatapp/Main.scala
+++ src/main/scala/com/example/chatapp/Main.scala
@@ -2,13 +2,13 @@ package com.example.chatapp
 
 import cats.effect.{ExitCode, IO, IOApp}
 import fs2.concurrent.Queue
+import fs2.Stream
 import org.http4s._
 import org.http4s.dsl.io._
 import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.websocket.WebSocketBuilder
-import fs2.Pipe
 import org.http4s.websocket.WebSocketFrame
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,27 +16,29 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
-    for {
-      q <- Queue.unbounded[IO, WebSocketFrame]
-      res <- startServer(q)
-    } yield res
-
-  }
-
-  def startServer(queue: Queue[IO, WebSocketFrame]): IO[ExitCode] = {
-    val receive: Pipe[IO, WebSocketFrame, Unit] =
-      s => s.map(x => {
-        queue.enqueue1(x).unsafeRunSync()
-      })
-
+    var allQueues: List[Queue[IO, WebSocketFrame]] = List.empty
     val helloWorldService = HttpRoutes.of[IO] {
       case GET -> Root / "hello" / name =>
         Ok(s"Hello, $name.")
       case GET -> Root / "ws" =>
-        WebSocketBuilder[IO].build(queue.dequeue, receive)
+        for {
+          queue <- Queue.unbounded[IO, WebSocketFrame]
+          _ <- IO {
+            allQueues ++= List(queue)
+          }
+          pipe = (s: Stream[IO, WebSocketFrame]) => s.map(x => {
+            println(x)
+            allQueues
+              .filter(_ != queue)
+              .foreach(_.enqueue1(x).unsafeRunSync())
+          })
+          response <- WebSocketBuilder[IO].build(queue.dequeue, pipe) //needs stream for sent messages and pipe to consume them
+
+        } yield response
     }
 
     val httpApp = Router("/" -> helloWorldService).orNotFound
+
     BlazeServerBuilder[IO](global).bindHttp(8080, "localhost")
       .withHttpApp(httpApp)
       .serve
